e176fb184a9e314d4563a924c8aa668b881ef14c
irico3 <iricocco3@gmail.com>
2023-06-28 13:10:27 +0900

    完成

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const transition = () => {
 
   startButton.addEventListener("click", () => {
     document.startViewTransition(() => {
-      diff1.hidden = false;
+      diff1.hidden = true;
       diff2.hidden = false;
     });
   });
@@ -34,3 +34,57 @@ const transition2 = () => {
 };
 
 transition2();
+
+const transition3 = () => {
+  const startButton3 = document.getElementById("startButton3");
+  assert(startButton3);
+  const diff1 = document.getElementById("difference3-1");
+  assert(diff1);
+  const diff2 = document.getElementById("difference3-2");
+  assert(diff2);
+
+  startButton3.addEventListener("click", () => {
+    diff1.classList.add("fadeOut");
+    diff2.classList.add("fadeIn");
+  });
+};
+
+transition3();
+
+const transition4 = () => {
+  const startButton4 = document.getElementById("startButton4");
+  assert(startButton4);
+  const diff1 = document.getElementById("difference4-1");
+  assert(diff1);
+  const diff2 = document.getElementById("difference4-2");
+  assert(diff2);
+  const container = document.getElementById("container");
+
+  startButton4.addEventListener("click", () => {
+    document.startViewTransition(() => {
+      diff1.hidden = true;
+      diff2.hidden = false;
+      container?.classList.add("is-zoomed");
+    });
+  });
+};
+
+transition4();
+
+const transition5 = () => {
+  const startButton4 = document.getElementById("startButton5");
+  assert(startButton4);
+  const diff1 = document.getElementById("difference5-1");
+  assert(diff1);
+  const diff2 = document.getElementById("difference5-2");
+  assert(diff2);
+
+  startButton4.addEventListener("click", () => {
+    document.startViewTransition(() => {
+      diff1.hidden = true;
+      diff2.hidden = false;
+    });
+  });
+};
+
+transition5();
